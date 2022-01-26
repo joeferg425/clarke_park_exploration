05a6c51526dac74d9a698f2b974b9fc328eea603
jf <jokester425@gmail.com>
2022-01-25 18:42:13 -0800

    working three-phase

--- test_helix.py
+++ test_helix.py
@@ -1,97 +1,29 @@
-import plotly.graph_objects as go
 import numpy as np
 import dash
 from dash import dcc
 from dash import html
 from dash.dependencies import Input, Output
-import plotly.express as px
-from sqlalchemy import true
 
 app = dash.Dash(__name__)
 
+two_pi = 2 * np.pi
+_120 = two_pi * (1 / 3)
+_240 = two_pi * (2 / 3)
 slider_count = 100
 sample_count = 100
-t = np.linspace(0, 1, sample_count)
-x = np.sin(2 * np.pi * t + (np.pi * (0)))
-y = np.cos(2 * np.pi * t + (np.pi * (0)))
+
+data = np.ones((3, 3, sample_count))
+data[:, :] *= np.linspace(0, 1, sample_count)
+data[:, [1, 2]] *= 2 * np.pi
 
 
-fig = go.Figure()
-fig.add_trace(
-    go.Scatter3d(
-        # df[mask],
-        x=x,
-        y=y,
-        z=t,
-        # marker={"size": 0},
-        # line={"color": "black"},
-        mode="lines",
-        # aspectmode="manual",
-        # aspectratio=[1, 0.2, 0.2],
-    )
-)
-fig.update_layout(
-    width=700,
-    height=700,
-    autosize=False,
-    # scene_aspectmode="cube",
-    # aspectratio=[1, 1, 1],
-    # aspectmode="manual",
-)
-fig.update_layout(
-    scene_aspectmode="manual",
-    scene_aspectratio=dict(x=1, y=1, z=1),
-)
-
-# fig.plotly_relayout(
-# {"scene_aspectmode": "cube"},
-# )
-
-# t = np.linspace(0, 1, sample_count)
-# for i, step in enumerate(np.linspace(0, two_pi, slider_count)):
-#     fig.add_trace(
-#         go.Scatter3d(
-#             visible=False,
-#             x=np.sin(2 * np.pi * t + (two_pi * (i / slider_count))),
-#             y=np.cos(2 * np.pi * t + (two_pi * (i / slider_count))),
-#             z=t,
-#             marker={"size": 0},
-#             line={"color": "black"},
-#             mode="lines",
-#         )
-#     )
-
-
-# Make first trace visible
-# fig.data[0].visible = True
-
-
-# Create and add slider
-# steps = []
-# for i in range(len(fig.data)):
-#     step = dict(
-#         method="update",
-#         args=[
-#             {"visible": [False] * len(fig.data)},
-#             {"title": "Slider switched to step: " + str(i)},
-#         ],  # layout attribute
-#     )
-#     step["args"][0]["visible"][i] = True  # Toggle i'th trace to "visible"
-#     steps.append(step)
-
-# sliders = [dict(active=slider_count, currentvalue={"prefix": "Frequency: "}, pad={"t": 50}, steps=steps)]
-
-# fig.update_layout(sliders=sliders)
-# fig.update_layout(scene_camera_eye=dict(x=-0.76, y=1.8, z=0.92))
 app.layout = html.Div(
     [
-        # html.H6("Change the value in the text box to see callbacks in action!"),
-        # html.Br(),
         html.Div(
             [
                 dcc.Graph(
                     id="scatter-plot",
-                    figure=fig,
+                    # figure=fig,
                     style={
                         "height": 700,
                         "width": 700,
@@ -105,9 +37,6 @@ app.layout = html.Div(
                 "scene_aspectmode": "cube",
             },
         ),
-        # html.Div([dcc.Graph(id="scatter-plot")], style={"height": 700, "width": 700}),
-        # html.Br(),
-        html.Div(["Input: ", dcc.Input(id="my-input", value="initial value", type="text")]),
         html.Div(
             [
                 dcc.Slider(
@@ -120,68 +49,38 @@ app.layout = html.Div(
                 )
             ]
         ),
-        # html.Br(),
-        # html.Div(id="my-output"),
     ],
-    # style={"height": "100vh"},
-    # scene={"scene_aspectmode": "cube"},
 )
-
-# fig.update_layout(
-#     width=800,
-#     height=700,
-#     autosize=False,
-#     scene_aspectmode="cube",
-#     # aspectratio=dict(x=1, y=1, z=1),
-#     # aspectmode="manual",
-# )
-
-
-# @app.callback(
-#     Output(component_id="my-output", component_property="children"),
-#     Input(component_id="my-input", component_property="value"),
-# )
-# def update_output_div(input_value):
-#     return "Output: {}".format(input_value)
 
 
 @app.callback(Output("scatter-plot", "figure"), [Input("range-slider", "value")])
 def update_bar_chart(slider_range):
-    print(slider_range)
-    # low, high = slider_range
-    # mask = (df.petal_width > low) & (df.petal_width < high)
-    x = np.sin(2 * np.pi * t + (np.pi * (slider_range)))
-    y = np.cos(2 * np.pi * t + (np.pi * (slider_range)))
-
-    # fig = px.scatter_3d(
-    # fig = go.Scatter3d(
-    # {"x": x, "y": y, "z": t},
-    # df[mask],
-    # x=x,
-    # y=y,
-    # z=t,
-    # marker={"size": 0},
-    # line={"color": "black"},
-    # mode="lines",
-    # style={
-    # height=700,
-    # width=700,
-    # mode="lines",
-    # "scene_aspectmode": "cube",
-    # },
-    # )
-    # return fig
-    # d = fig.
-    # return d
     return {
         "data": [
             {
-                "x": x,
-                "y": y,
-                "z": t,
+                "x": data[0, 0],
+                "y": np.sin(data[0, 1] + (slider_range * 2 * np.pi)),
+                "z": np.cos(data[0, 2] + (slider_range * 2 * np.pi)),
                 "type": "scatter3d",
                 "mode": "lines",
-            }
+                "name": "Phase A",
+            },
+            {
+                "x": data[1, 0],
+                "y": np.sin(data[1, 1] + (slider_range * 2 * np.pi) + _120),
+                "z": np.cos(data[1, 2] + (slider_range * 2 * np.pi) + _120),
+                "type": "scatter3d",
+                "mode": "lines",
+                "name": "Phase B",
+            },
+            {
+                "x": data[2, 0],
+                "y": np.sin(data[2, 1] + (slider_range * 2 * np.pi) + _240),
+                "z": np.cos(data[2, 2] + (slider_range * 2 * np.pi) + _240),
+                "type": "scatter3d",
+                "mode": "lines",
+                "name": "Phase C",
+            },
         ],
         "layout": {
             "height": 700,
@@ -200,8 +99,4 @@ def update_bar_chart(slider_range):
     }
 
 
-# app.
-
-
-# fig.show()
 app.run_server(debug=True)

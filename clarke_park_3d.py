6e61e121ce02b615da29499674324e85f1ce6039
jf <jokester425@gmail.com>
2022-01-25 20:41:51 -0800

    Clarke transform is plotting

--- clarke_park_3d.py
+++ clarke_park_3d.py
@@ -6,6 +6,8 @@ from dash import html
 from dash.dependencies import Input, Output
 from enum import IntEnum
 
+from sqlalchemy import false
+
 
 class FocusAxis(IntEnum):
     XY = 0
@@ -24,13 +26,136 @@ _120 = two_pi * (1 / 3)
 _240 = two_pi * (2 / 3)
 slider_count = 100
 sample_count = 100
+first = True
 
 data = np.ones((3, 3, sample_count))
 data[:, :] *= np.linspace(0, 1, sample_count)
 data[:, [1, 2]] *= 2 * np.pi
-
+zeros = np.zeros((sample_count))
 radian_offset = 0
 fig = None
+
+
+def do_clarke_transform():
+    """Perform Clarke transform function.
+
+    https://en.wikipedia.org/wiki/Alpha%E2%80%93beta_transformation
+    https://www.mathworks.com/help/physmod/sps/ref/clarketransform.html
+    """
+    global data
+    # Clarke transform
+    clarke_matrix = (2 / 3) * np.array(
+        [
+            [1, -(1 / 2), -(1 / 2)],
+            [0, (np.sqrt(3) / 2), -(np.sqrt(3) / 2)],
+            [(1 / 2), (1 / 2), (1 / 2)],
+        ]
+    )
+    # Clarke transform function
+    clarke_alpha_beta_zero_array = np.dot(
+        clarke_matrix,
+        np.array(
+            [
+                np.sin(data[0, 1, :]),
+                np.sin(data[1, 1, :] + (radian_offset * 2 * np.pi) + _120),
+                np.sin(data[2, 1, :] + (radian_offset * 2 * np.pi) + _240),
+            ]
+        )
+        # * np.array([self.amplitude1, self.amplitude2, self.amplitude3])[:, None],
+    )
+    # assign outputs to individual variables for polar plots
+    # (
+    #     self.clarke_alpha_instantaneous,
+    #     self.clarke_beta_instantaneous,
+    #     self.clarke_zero_instantaneous,
+    # ) = self.clarke_alpha_beta_zero_array[:, 0]
+    # # matplotlib likes positive vector lengths
+    # if self.clarke_alpha_instantaneous >= 0:
+    #     self.clarke_alpha_instantaneous_angle = 0.0
+    # else:
+    #     self.clarke_alpha_instantaneous_angle = np.pi
+    #     self.clarke_alpha_instantaneous *= -1
+    # if self.clarke_beta_instantaneous >= 0:
+    #     self.clarke_beta_instantaneous_angle = np.pi * 3 / 2
+    # else:
+    #     self.clarke_beta_instantaneous_angle = np.pi / 2
+    #     self.clarke_beta_instantaneous *= -1
+    return clarke_alpha_beta_zero_array
+
+
+# def do_park_transform(self):
+#     """Perform Park transform function.
+
+#     https://de.wikipedia.org/wiki/D/q-Transformation
+#     https://www.mathworks.com/help/physmod/sps/ref/clarketoparkangletransform.html
+#     """
+#     # create Park transformation matrix, with reference based on enum value
+#     if self.park_alignment == ParkAlignment.q_aligned:
+#         self.park_matrix = np.array(
+#             [
+#                 [
+#                     np.sin(self.three_phase_data_matrix[PhaseIndex.Phase1, :]),
+#                     -np.cos(self.three_phase_data_matrix[PhaseIndex.Phase1, :]),
+#                     np.zeros((self.sample_count)),
+#                 ],
+#                 [
+#                     np.cos(self.three_phase_data_matrix[PhaseIndex.Phase1, :]),
+#                     np.sin(self.three_phase_data_matrix[PhaseIndex.Phase1, :]),
+#                     np.zeros((self.sample_count)),
+#                 ],
+#                 [
+#                     np.zeros((self.sample_count)),
+#                     np.zeros((self.sample_count)),
+#                     np.ones((self.sample_count)),
+#                 ],
+#             ]
+#         )
+#     elif self.park_alignment == ParkAlignment.d_aligned:
+#         self.park_matrix = np.array(
+#             [
+#                 [
+#                     np.sin(self.three_phase_data_matrix[PhaseIndex.Phase1, :]),
+#                     np.cos(self.three_phase_data_matrix[PhaseIndex.Phase1, :]),
+#                     np.zeros((self.sample_count)),
+#                 ],
+#                 [
+#                     np.cos(self.three_phase_data_matrix[PhaseIndex.Phase1, :]),
+#                     -np.sin(self.three_phase_data_matrix[PhaseIndex.Phase1, :]),
+#                     np.zeros((self.sample_count)),
+#                 ],
+#                 [
+#                     np.zeros((self.sample_count)),
+#                     np.zeros((self.sample_count)),
+#                     np.ones((self.sample_count)),
+#                 ],
+#             ]
+#         )
+#     # perform the matrix math
+#     self.park_array = np.einsum(
+#         "ijk,ik->jk",
+#         self.park_matrix,
+#         self.clarke_alpha_beta_zero_array,
+#     )
+#     # save instantaneous values for polar plots
+#     (
+#         self.park_d_instantaneous,
+#         self.park_q_instantaneous,
+#         self.park_zero_instantaneous,
+#     ) = self.park_array[:, 0]
+#     # set the phase angle based on reference value
+#     if self.park_alignment == ParkAlignment.q_aligned:
+#         self.park_q_instantaneous_angle = self.three_phase_data_matrix[PhaseIndex.Phase1, 0]
+#         self.park_d_instantaneous_angle = self.three_phase_data_matrix[PhaseIndex.Phase1, 0] - np.pi / 2
+#     else:
+#         self.park_d_instantaneous_angle = self.three_phase_data_matrix[PhaseIndex.Phase1, 0]
+#         self.park_q_instantaneous_angle = self.three_phase_data_matrix[PhaseIndex.Phase1, 0] + np.pi / 2
+#     # matplotlib likes positive vectors
+#     if self.park_d_instantaneous < 0:
+#         self.park_d_instantaneous *= -1
+#         self.park_d_instantaneous_angle += 2 * np.pi
+#     if self.park_q_instantaneous < 0:
+#         self.park_q_instantaneous *= -1
+#         self.park_q_instantaneous_angle += 2 * np.pi
 
 
 app.layout = html.Div(
@@ -77,37 +202,116 @@ app.layout = html.Div(
 
 
 def generate_figure_data():
-    global data, radian_offset, focus_selection
+    global data, radian_offset, focus_selection, first
+    clarke = do_clarke_transform()
     figure_data = {
         "data": [
             {
-                "x": data[0, 0],
-                "y": np.sin(data[0, 1] + (radian_offset * 2 * np.pi)),
-                "z": np.cos(data[0, 2] + (radian_offset * 2 * np.pi)),
+                "x": data[0, 0, :],
+                "y": np.sin(data[0, 1, :] + (radian_offset * 2 * np.pi)),
+                "z": np.cos(data[0, 2, :] + (radian_offset * 2 * np.pi)),
                 "type": "scatter3d",
                 "mode": "lines",
                 "name": "Phase A",
             },
             {
-                "x": data[1, 0],
-                "y": np.sin(data[1, 1] + (radian_offset * 2 * np.pi) + _120),
-                "z": np.cos(data[1, 2] + (radian_offset * 2 * np.pi) + _120),
+                "x": data[1, 0, :],
+                "y": np.sin(data[1, 1, :] + (radian_offset * 2 * np.pi) + _120),
+                "z": np.cos(data[1, 2, :] + (radian_offset * 2 * np.pi) + _120),
                 "type": "scatter3d",
                 "mode": "lines",
                 "name": "Phase B",
             },
             {
-                "x": data[2, 0],
-                "y": np.sin(data[2, 1] + (radian_offset * 2 * np.pi) + _240),
-                "z": np.cos(data[2, 2] + (radian_offset * 2 * np.pi) + _240),
+                "x": data[2, 0, :],
+                "y": np.sin(data[2, 1, :] + (radian_offset * 2 * np.pi) + _240),
+                "z": np.cos(data[2, 2, :] + (radian_offset * 2 * np.pi) + _240),
                 "type": "scatter3d",
                 "mode": "lines",
                 "name": "Phase C",
             },
+            {
+                "x": [0, 0],
+                "y": [0, np.sin(data[0, 1, 0] + (radian_offset * 2 * np.pi))],
+                "z": [0, np.cos(data[0, 2, 0] + (radian_offset * 2 * np.pi))],
+                "type": "scatter3d",
+                "mode": "lines",
+                "name": "Phasor A",
+            },
+            {
+                "x": [0, 0],
+                "y": [0, np.sin(data[1, 1, 0] + (radian_offset * 2 * np.pi) + _120)],
+                "z": [0, np.cos(data[1, 2, 0] + (radian_offset * 2 * np.pi) + _120)],
+                "type": "scatter3d",
+                "mode": "lines",
+                "name": "Phasor B",
+            },
+            {
+                "x": [0, 0],
+                "y": [0, np.sin(data[2, 1, 0] + (radian_offset * 2 * np.pi) + _240)],
+                "z": [0, np.cos(data[2, 2, 0] + (radian_offset * 2 * np.pi) + _240)],
+                "type": "scatter3d",
+                "mode": "lines",
+                "name": "Phasor C",
+            },
+            {
+                "x": data[0, 0, :],
+                "y": clarke[0, :],
+                "z": zeros,
+                "type": "scatter3d",
+                "mode": "lines",
+                "name": "Clarke α(t)",
+            },
+            {
+                "x": data[0, 0, :],
+                "y": zeros,
+                "z": clarke[1, :],
+                "type": "scatter3d",
+                "mode": "lines",
+                "name": "Clarke β(t)",
+            },
+            {
+                "x": [0, 0],
+                "y": [0, clarke[0, 0]],
+                "z": [0, 0],
+                "type": "scatter3d",
+                "mode": "lines",
+                "name": "Clarke α",
+            },
+            {
+                "x": [0, 0],
+                "y": [0, 0],
+                "z": [0, clarke[1, 0]],
+                "type": "scatter3d",
+                "mode": "lines",
+                "name": "Clarke β",
+            },
         ],
-        "layout": {
+    }
+    if first is False:
+        figure_data["layout"] = {
+            "uirevision": 1,
+            "xaxis": {"range": [-1, 1]},
+            "yaxis": {"range": [-1, 1]},
+            "zaxis": {"range": [-1, 1]},
+            "scene": {
+                # "aspectmode": "manual",
+                "aspectratio": {
+                    "x": 1,
+                    "y": 1,
+                    "z": 1,
+                },
+            },
+        }
+    else:
+        first = False
+        figure_data["layout"] = {
+            "uirevision": 1,
             "height": 700,
             "width": 700,
+            "xaxis": {"range": [-1, 1]},
+            "yaxis": {"range": [-1, 1]},
+            "zaxis": {"range": [-1, 1]},
             "scene_aspectmode": "cube",
             "autosize": False,
             "scene": {
@@ -118,8 +322,8 @@ def generate_figure_data():
                     "z": 1,
                 },
             },
-        },
-    }
+        }
+
     if focus_selection == FocusAxis.XYZ:
         figure_data["layout"]["scene"]["camera"] = {
             "up": {
@@ -182,7 +386,7 @@ def generate_figure_data():
     #         },
     #     }
 
-    focus_selection = FocusAxis.NONE
+    # focus_selection = FocusAxis.NONE
 
     return figure_data
 
